b84d9b96104e66c102a98ca941c77cd1f22596cd
Jimmy <odylic.lin@gmail.com>
2021-08-01 14:38:27 -0700

    Added increment by amount functionality

--- src/redux-toolkit/components/counter.js
+++ src/redux-toolkit/components/counter.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  increment,
+  decrement,
+  selectCounter,
+  incrementByAmount,
+} from "../store/counterSlice";
 
 const containerStyle = {
   display: "flex",
@@ -10,20 +17,45 @@ const buttonStyle = {
 };
 
 export default function counter() {
+  const dispatch = useDispatch();
+  const count = useSelector(selectCounter);
+  const [incrementAmount, setIncrementAmount] = useState("2");
+  const resetInput = (e) => {
+    e.target.value = "";
+  };
+
+  const incrementValue = Number(incrementAmount) || 0;
+
   return (
     <div className="App">
       <header className="App-header">
         Redux ToolKit
-        <h1>0</h1>
+        <h1>{count} </h1>
         <div style={containerStyle}>
-          <button type="button" style={buttonStyle}>
+          <button
+            type="button"
+            onClick={() => dispatch(decrement())}
+            style={buttonStyle}
+          >
             -
           </button>
-          <button type="button" style={buttonStyle}>
+          <button
+            type="button"
+            onClick={() => dispatch(increment())}
+            style={buttonStyle}
+          >
             +
           </button>
         </div>
       </header>
+      <input
+        value={incrementAmount}
+        onFocus={(e) => resetInput(e)}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+      <button onClick={() => dispatch(incrementByAmount(incrementValue))}>
+        Add Amount
+      </button>
     </div>
   );
 }
--- src/redux-toolkit/store/counterSlice.js
+++ src/redux-toolkit/store/counterSlice.js
@@ -1,26 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  count: 0
-}
+  count: 0,
+};
 
-const counterSlice = createSlice({
-  name: 'counter',
+export const counterSlice = createSlice({
+  name: "counter",
   initialState,
   reducers: {
     increment: (state, action) => {
-      state.count += 1
+      state.count += 1;
     },
     decrement: (state, action) => {
-      state.count -= 1
-    }
+      state.count -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload;
+    },
+  },
+});
 
-  }
-})
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 
+export const selectCounter = (state) => state.counter.count;
 
-export const { increment, decrement} = counterSlice.actions;
-
-export default counterSlice.reducers;
-
-
+// export default reducer, not reducers to the store
+export default counterSlice.reducer;
